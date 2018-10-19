5c4a60db95b6508f03c29016e4c6d33ae8ed14ab
Anton Medvedev <anton@medv.io>
2018-10-19 18:34:26 +0700

    Update bench

--- bench_test.go
+++ bench_test.go
@@ -6,35 +6,92 @@ import (
 	"github.com/antonmedv/expr"
 )
 
-type segment struct {
+type Segment struct {
 	Origin string
 }
-type passengers struct {
+type Passengers struct {
 	Adults int
 }
-type request struct {
-	Segments   []*segment
-	Passengers *passengers
+type Env struct {
+	Segments   []Segment
+	Passengers Passengers
 	Marker     string
 }
 
-func Benchmark_expr(b *testing.B) {
-	r := &request{
-		Segments: []*segment{
-			{Origin: "MOW"},
-		},
-		Passengers: &passengers{
-			Adults: 2,
-		},
-		Marker: "test",
-	}
+func (e *Env) First(s []Segment) string {
+	return s[0].Origin
+}
 
-	script, err := expr.Parse(`Segments[0].Origin == "MOW" && Passengers.Adults == 2 && Marker == "test"`)
+var env = Env{
+	Segments: []Segment{
+		{Origin: "LED"},
+		{Origin: "HKT"},
+	},
+	Passengers: Passengers{
+		Adults: 2,
+	},
+	Marker: "test",
+}
+
+func Benchmark_struct(b *testing.B) {
+	program, err := expr.Parse(
+		`Segments[0].Origin == "LED" && Passengers.Adults == 2 && Marker == "test"`,
+		expr.Env(Env{}),
+	)
 	if err != nil {
 		b.Fatal(err)
 	}
+	out, err := expr.Run(program, env)
+	if err != nil {
+		panic(err)
+	}
+	if !out.(bool) {
+		panic("unexpected result")
+	}
 
 	for n := 0; n < b.N; n++ {
-		expr.Run(script, r)
+		expr.Run(program, env)
+	}
+}
+
+func Benchmark_map(b *testing.B) {
+	env := map[string]interface{}{
+		"segments":   env.Segments,
+		"passengers": env.Passengers,
+		"marker":     env.Marker,
+	}
+
+	program, err := expr.Parse(`segments[0].Origin == "LED" && passengers.Adults == 2 && marker == "test"`)
+	if err != nil {
+		b.Fatal(err)
+	}
+	out, err := expr.Run(program, env)
+	if err != nil {
+		panic(err)
+	}
+	if !out.(bool) {
+		panic("unexpected result")
+	}
+
+	for n := 0; n < b.N; n++ {
+		expr.Run(program, env)
+	}
+}
+
+func Benchmark_func(b *testing.B) {
+	program, err := expr.Parse(`First(Segments)`, expr.Env(&Env{}))
+	if err != nil {
+		b.Fatal(err)
+	}
+	out, err := expr.Run(program, &env)
+	if err != nil {
+		panic(err)
+	}
+	if out.(string) != "LED" {
+		panic("unexpected result")
+	}
+
+	for n := 0; n < b.N; n++ {
+		expr.Run(program, &env)
 	}
 }

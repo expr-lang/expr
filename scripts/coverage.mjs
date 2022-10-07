e8e8171d4f3f06836133a52da8a5bb666efdb4ed
Anton Medvedev <anton@medv.io>
2022-10-07 22:32:29 +0200

    Better coverage.mjs

--- scripts/coverage.mjs
+++ scripts/coverage.mjs
@@ -1,10 +1,15 @@
-#!/usr/bin/env zx
-cd(path.join(__dirname, '..'))
-await $`go test -coverprofile=coverage.out -coverpkg=github.com/antonmedv/expr/... ./...`
-const coverage = fs.readFileSync('coverage.out').toString()
-  .split('\n')
-  .filter(line => !line.match(/cmd|generate/))
-  .join('\n')
-fs.writeFileSync('coverage.out', coverage)
-await $`go tool cover -html=coverage.out -o coverage.html`
+#!/usr/bin/env zx --experimental
+
+cd(path.resolve(__dirname, '..'))
+
+await spinner('Running tests', async () => {
+  await $`go test -coverprofile=coverage.out -coverpkg=github.com/antonmedv/expr/... ./...`
+  const coverage = fs.readFileSync('coverage.out').toString()
+    .split('\n')
+    .filter(line => !line.match(/cmd|generate/))
+    .join('\n')
+  fs.writeFileSync('coverage.out', coverage)
+  await $`go tool cover -html=coverage.out -o coverage.html`
+})
+
 await $`go tool cover -func=coverage.out`

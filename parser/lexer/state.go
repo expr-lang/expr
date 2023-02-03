4c2919970f268cb7beeeff124193ab9e7e7efb44
Anton Medvedev <anton@medv.io>
2023-02-03 14:46:15 +0100

    Lexer: remove redundant case for `?`.

--- parser/lexer/state.go
+++ parser/lexer/state.go
@@ -32,7 +32,7 @@ func root(l *lexer) stateFn {
 		l.emit(Bracket)
 	case strings.ContainsRune(")]}", r):
 		l.emit(Bracket)
-	case strings.ContainsRune("#,?:%+-^", r): // single rune operator
+	case strings.ContainsRune("#,:%+-^", r): // single rune operator
 		l.emit(Operator)
 	case strings.ContainsRune("&|!=*<>", r): // possible double rune operator
 		l.accept("&|=*")

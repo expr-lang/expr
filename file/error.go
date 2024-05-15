bf14f7f118939af3ab5f3030b39954e8661aa6ae
Anton Medvedev <anton@medv.io>
2024-05-15 23:43:15 +0200

    Refactor lexer

--- file/error.go
+++ file/error.go
@@ -8,6 +8,8 @@ import (
 
 type Error struct {
 	Location
+	Line    int    `json:"line"`
+	Column  int    `json:"column"`
 	Message string `json:"message"`
 	Snippet string `json:"snippet"`
 	Prev    error  `json:"prev"`
@@ -18,12 +20,24 @@ func (e *Error) Error() string {
 }
 
 func (e *Error) Bind(source Source) *Error {
-	if snippet, found := source.Snippet(e.Location.Line); found {
+	e.Line = 1
+	for i, r := range source {
+		if i == e.From {
+			break
+		}
+		if r == '\n' {
+			e.Line++
+			e.Column = 0
+		} else {
+			e.Column++
+		}
+	}
+	if snippet, found := source.Snippet(e.Line); found {
 		snippet := strings.Replace(snippet, "\t", " ", -1)
 		srcLine := "\n | " + snippet
 		var bytes = []byte(snippet)
 		var indLine = "\n | "
-		for i := 0; i < e.Location.Column && len(bytes) > 0; i++ {
+		for i := 0; i < e.Column && len(bytes) > 0; i++ {
 			_, sz := utf8.DecodeRune(bytes)
 			bytes = bytes[sz:]
 			if sz > 1 {
@@ -54,7 +68,7 @@ func (e *Error) Wrap(err error) {
 }
 
 func (e *Error) format() string {
-	if e.Location.Empty() {
+	if e.Snippet == "" {
 		return e.Message
 	}
 	return fmt.Sprintf(
--- file/location.go
+++ file/location.go
@@ -1,12 +1,6 @@
 package file
 
 type Location struct {
-	Line   int `json:"line"`   // The 1-based line of the location.
-	Column int `json:"column"` // The 0-based column number of the location.
-	From   int `json:"from"`   // The 0-based byte offset from the beginning.
-	To     int `json:"to"`     // The 0-based byte offset to the end.
-}
-
-func (l Location) Empty() bool {
-	return l.Column == 0 && l.Line == 0
+	From int `json:"from"`
+	To   int `json:"to"`
 }

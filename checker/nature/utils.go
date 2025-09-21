55acb0e2b191d600bc79f578d6f6b477cdb06cd3
Diego Augusto Molina <diegoaugustomolina@gmail.com>
2025-09-21 01:23:17 -0300

    fix #844: remove incorrect bug notice

--- checker/nature/utils.go
+++ checker/nature/utils.go
@@ -52,8 +52,6 @@ func (s *structData) structField(c *Cache, parentEmbed *structData, name string)
 	// Lookup own fields first.
 	for ; s.ownIdx < s.numField; s.ownIdx++ {
 		field := s.rType.Field(s.ownIdx)
-		// BUG: we should skip if !field.IsExported() here
-
 		if field.Anonymous && s.anonIdx < 0 {
 			// start iterating anon fields on the first instead of zero
 			s.anonIdx = s.ownIdx
